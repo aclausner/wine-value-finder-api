669ac55f241b154c017754aa644c18d51693829f
aclausner <adamclausner@gmail.com>
2026-05-11 12:31:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*', methods: ['GET', 'POST', 'OPTIONS'], allowedHeaders: ['Content-Type'] }));
 app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
